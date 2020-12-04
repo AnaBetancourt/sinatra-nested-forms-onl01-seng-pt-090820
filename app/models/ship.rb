eea161c3a95d254e97cbee81719f112dbb28697f
AnaBetancourt <anaa.betancourt13@gmail.com>
2020-12-04 14:24:38 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize (name, type, booty)
+        @name = name
+        @typet = type
+        @booty = booty
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        Ship.all.delete
+    end
+    
 end
